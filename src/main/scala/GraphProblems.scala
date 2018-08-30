041522fd3005d0f283206a166f8b711aa8a0cd99
Alexander <alexsniffin@hotmail.com>
2018-08-30 14:42:48 -0500

    fix bugs

--- src/main/scala/GraphProblems.scala
+++ src/main/scala/GraphProblems.scala
@@ -1,5 +1,5 @@
 import scala.annotation.tailrec
-import scala.collection.immutable.Queue
+import scala.collection.mutable
 
 case class Node[T](value: T, left: Option[Node[T]], right: Option[Node[T]])
 
@@ -9,7 +9,8 @@ object GraphProblems {
 	  * breadth first search for tree
 	  */
 	def searchTree[T](tree: Node[T], value: T): Option[T] = {
-		var queue: Queue[Node[T]] = Queue[Node[T]](tree)
+		// side-effects contained only in this function
+		val queue: mutable.Queue[Option[Node[T]]] = mutable.Queue[Option[Node[T]]]()
 
 		/**
 		  * tail recursion
@@ -22,18 +23,13 @@ object GraphProblems {
 				queue.enqueue(node.left)
 				queue.enqueue(node.right)
 
-				val next = queue.dequeue
-
-				search(Option(next._1))
+				search(queue.dequeue)
 			}
-			case None => None
+			case None => if (queue.nonEmpty) search(queue.dequeue) else None
 			case _ => None
 		}
 
-		val dequeue = queue.dequeue
-		queue = dequeue._2
-
-		Option(search(Option(dequeue._1)).get.value)
+		Option(search(Option(tree)).get.value)
 	}
 
 }
