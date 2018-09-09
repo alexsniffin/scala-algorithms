92cacfe60198525ecd54bf09c9b98bc0ccc0b701
Alexander <alexsniffin@hotmail.com>
2018-09-08 19:18:57 -0500

    Add dfs

--- src/main/scala/SearchProblems.scala
+++ src/main/scala/SearchProblems.scala
@@ -1,5 +1,7 @@
 import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.collection.immutable.{HashSet, List}
+import scala.language.postfixOps
 
 case class TreeNode[T](value: T, left: Option[TreeNode[T]] = None, right: Option[TreeNode[T]] = None)
 case class GraphNode[T](value: T, edges: Option[Seq[Option[GraphNode[T]]]] = None)
@@ -64,6 +66,86 @@ object SearchProblems {
                     search(queue.dequeue)
                 }
             case None => if (queue.nonEmpty) search(queue.dequeue) else None
+            case _ => None
+        }
+
+        Option(search(Option(graph))).flatten
+    }
+
+    /***
+      * Tail-recursive depth first search for a binary tree
+      * @param tree Node at the lowest level
+      * @param value Value to find
+      * @tparam T Type of the `value`
+      * @return Option with the node
+      */
+    def dfsTreeSearch[T](tree: TreeNode[T], value: T): Option[TreeNode[T]] = {
+        var stack: List[Option[TreeNode[T]]] = List[Option[TreeNode[T]]]()
+
+        /**
+          * Tail recursion search
+          */
+        @tailrec
+        def search(curNode: Option[TreeNode[T]]): Option[TreeNode[T]] = curNode match {
+            case Some(node) =>
+                if (node.value.equals(value)) return Option(node)
+
+                // Append child nodes
+                stack = node.left :: node.right :: stack
+
+                val result = stack.head
+                stack = stack.tail
+
+                search(result)
+            case None => if (stack.nonEmpty) {
+                val result = stack.head
+                stack = stack.tail
+
+                search(result)
+            } else None
+            case _ => None
+        }
+
+        Option(search(Option(tree))).flatten
+    }
+
+    /***
+      * Tail-recursive depth first search for a graph
+      * @param graph Entry node
+      * @param value Value to find
+      * @tparam T Type of the `value`
+      * @return Option with the node
+      */
+    def dfsGraphSearch[T](graph: GraphNode[T], value: T): Option[GraphNode[T]] = {
+        // Side-effects contained only in this function scope
+        var stack: List[Option[GraphNode[T]]] = List[Option[GraphNode[T]]]()
+        var visited: HashSet[T] = HashSet[T]()
+
+        /**
+          * Tail recursion search
+          */
+        @tailrec
+        def search(curNode: Option[GraphNode[T]]): Option[GraphNode[T]] = curNode match {
+            case Some(node) =>
+                if (node.value.equals(value)) return Option(node)
+
+                if (!visited.contains(node.value) && node.edges.isDefined)
+                    stack = node.edges.get.toList ::: stack
+
+                if (stack.isEmpty) None else {
+                    visited += node.value
+
+                    val result = stack.head
+                    stack = stack.tail
+
+                    search(result)
+                }
+            case None => if (stack.nonEmpty) {
+                val result = stack.head
+                stack = stack.tail
+
+                search(result)
+            } else None
             case _ => None
         }
 
--- src/test/scala/SearchProblemsSpec.scala
+++ src/test/scala/SearchProblemsSpec.scala
@@ -77,4 +77,78 @@ class SearchProblemsSpec extends FlatSpec  with MockFactory with GivenWhenThen {
         Then("the result should be none")
         assert(result.isEmpty)
     }
+
+    "DFS for trees" should "find the given value" in {
+        Given("a tree with nodes")
+        val findValue = 1
+        val treeFixture = new TreeNode[Int](0, right = Option(new TreeNode[Int](2, right = Option(new TreeNode[Int](1)))))
+
+        When("searching the tree for a value")
+        val result = SearchProblems.dfsTreeSearch[Int](treeFixture, findValue)
+
+        Then("the result should be the value")
+        assert(result.get.value.equals(findValue))
+    }
+
+    "DFS for trees" should "not find the given value" in {
+        Given("a tree with nodes")
+        val findValue = 1
+        val treeFixture = new TreeNode[Int](0, right = Option(new TreeNode[Int](2, right = Option(new TreeNode[Int](3)))))
+
+        When("searching the tree for a value")
+        val result = SearchProblems.dfsTreeSearch[Int](treeFixture, findValue)
+
+        Then("the result should be none")
+        assert(result.isEmpty)
+    }
+
+    "DFS for graphs" should "find the given value" in {
+        //work around for mock
+        //https://github.com/paulbutcher/ScalaMock/issues/221
+
+
+        Given("a graph with nodes")
+        val findValue = 1
+        val graphFixture = new GraphNode[Int](0, Option(Seq(
+            Option(new GraphNode[Int](3)),
+            Option(new GraphNode[Int](4)),
+            Option(new GraphNode[Int](6, Option(Seq(
+                Option(new GraphNode[Int](8)),
+                Option(new GraphNode[Int](2, Option(Seq(
+                    Option(new GraphNode[Int](1)),
+                    Option(new GraphNode[Int](10))
+                )))),
+                Option(new GraphNode[Int](9))
+            )))))
+        ))
+
+        When("searching the graph for a value")
+        val result = SearchProblems.dfsGraphSearch[Int](graphFixture, findValue)
+
+        Then("the result should be the value")
+        assert(result.get.value.equals(findValue))
+    }
+
+    "DFS for graphs" should "not find the given value" in {
+        Given("a graph with nodes")
+        val findValue = 1
+        val graphFixture = new GraphNode[Int](0, Option(Seq(
+            Option(new GraphNode[Int](3)),
+            Option(new GraphNode[Int](4)),
+            Option(new GraphNode[Int](6, Option(Seq(
+                Option(new GraphNode[Int](8)),
+                Option(new GraphNode[Int](2, Option(Seq(
+                    Option(new GraphNode[Int](11)),
+                    Option(new GraphNode[Int](10))
+                )))),
+                Option(new GraphNode[Int](9))
+            )))))
+        ))
+
+        When("searching the graph for a value")
+        val result = SearchProblems.dfsGraphSearch[Int](graphFixture, findValue)
+
+        Then("the result should be none")
+        assert(result.isEmpty)
+    }
 }
