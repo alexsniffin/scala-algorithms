1786e09af62111a223596f99f02b23813bcf12a5
Alexander Sniffin <alexsniffin@hotmail.com>
2018-09-28 10:14:23 -0400

    Simplify and use logic as an expression for result

--- src/main/scala/StringProblems.scala
+++ src/main/scala/StringProblems.scala
@@ -12,18 +12,17 @@ object StringProblems {
         val memoizedResults = Array.ofDim[Option[Int]](first.length + 1, second.length + 1)
 
         def compare(pos1: Int, pos2: Int): Int = {
-            var result = 0
-
-            if (memoizedResults.length > pos1 &&
+            val result: Int = if (memoizedResults.length > pos1 &&
                 memoizedResults(pos1).length > pos2 &&
 	            memoizedResults(pos1)(pos2) != null)
-                result = memoizedResults(pos1)(pos2).get
+                memoizedResults(pos1)(pos2).get
             else if (pos1 == 0 || pos2 == 0)
-                result = 0
+                0
             else if (first.charAt(pos1 - 1).equals(second.charAt(pos2 - 1)))
-                result = 1 + compare(pos1 - 1, pos2 - 1)
+                1 + compare(pos1 - 1, pos2 - 1)
             else if (!first.charAt(pos1 - 1).equals(second.charAt(pos2 - 1)))
-                result = compare(pos1 - 1, pos2).max(compare(pos1, pos2 - 1))
+                compare(pos1 - 1, pos2).max(compare(pos1, pos2 - 1))
+            else 0
 
             memoizedResults(pos1)(pos2) = Option(result)
             result
